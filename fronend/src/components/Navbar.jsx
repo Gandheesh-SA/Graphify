c2731d61cd02475239f8169c73a6a6e1f56a4239
Gandheesh-SA <sagandheesh1911@gmail.com>
2025-11-05 12:15:18 +0530

    Updated Navbar

--- fronend/src/components/Navbar.jsx
+++ fronend/src/components/Navbar.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../styles/navbar.css"
+import "../styles/navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <h1>Graphify</h1>
-      <div>
-        <Link to="/create">Create</Link>
-        <Link to="/bfs">BFS</Link>
-        <Link to="/dfs">DFS</Link>
-        <Link to="/dijkstra">Dijkstra</Link>
-        <Link to="/kruskal">Kruskal</Link>
+
+      {/* Nav links container with correct class */}
+      <div className={`nav-links ${isOpen ? "active" : ""}`}>
+        <Link to="/create" onClick={() => setIsOpen(false)}>Create</Link>
+        <Link to="/bfs" onClick={() => setIsOpen(false)}>BFS</Link>
+        <Link to="/dfs" onClick={() => setIsOpen(false)}>DFS</Link>
+        <Link to="/dijkstra" onClick={() => setIsOpen(false)}>Dijkstra</Link>
+        <Link to="/kruskal" onClick={() => setIsOpen(false)}>Kruskal</Link>
+      </div>
+
+      {/* Hamburger Icon */}
+      <div className={`menu-icon ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </nav>
   );
